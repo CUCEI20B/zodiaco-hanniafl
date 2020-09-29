51ab0932e4a9ef49610539524c39f25b788566ed
Hannia Flores <hannia.flores2958@alumnos.udg.mx>
2020-09-29 17:21:50 -0500

    prueba 02

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
-signo = ["capricornio", "acuario", "piscis", "aries", "tauro", "geminis", "cancer", "leo", "virgo", "libra", "escorpio", "sagitario" ]
-fecha = [20, 19, 20, 21, 21, 22, 22, 22, 22, 22, 21]
+dia=mes=0
 
-#Pidiendo datos al usuario 
-mes = int(input("Digite su mes de nacimiento: "))
-dia = int(input("Dia de nacimiento: ") )
+dia=int(input("Ingresa tu dia de nacimiento: "))
+mes=int(input("Ingresa tu mes de nacimiento: "))
 
-mes = mes -1
-if dia > fecha[mes]:
-    mes = mes + 1
-
-if mes == 12:
-    mes = 0 
-print ("Su signo zodiacal es:", signo[mes])
+if dia>=22 and dia<=31 and mes==12:
+    print("Capricornio")
+elif dia<=20 and dia>=1 and mes==1:
+    print("Capricornio")
+if dia>=21 and dia<=31 and mes==1:
+    print("Acuario")
+elif dia<=19 and dia>=1 and mes==2:
+    print("Acuario")
+if dia>=20 and dia<=28 and mes==2:
+    print("Piscis")
+elif dia<=20 and dia>=1 and mes==3:
+    print("Piscis")
+if dia>=21 and dia<=31 and mes==3:
+    print("Aries")
+elif dia<=20 and dia>=1 and mes==4:
+    print("Aries")
+if dia>=21 and dia<=30 and mes==4:
+    print("Tauro")
+elif dia<=21 and dia>=1 and mes==5:
+    print("Tauro")
+if dia>=22 and dia<=31 and mes==5:
+    print("Geminis")
+elif dia<=21 and dia>=1 and mes==6:
+    print("Geminis")
+if dia>=22 and dia<=30 and mes==6:
+    print("Cancer")
+elif dia<=22 and dia>=1 and mes==7:
+    print("Cancer")
+if dia>=23 and dia<=31 and mes==7:
+    print("Leo")
+elif dia<=23 and dia>=1 and mes==8:
+    print("Leo")
+if dia>=24 and dia<=31 and mes==8:
+    print("Virgo")
+elif dia<=22 and dia>=1 and mes==9:
+    print("Virgo")
+if dia>=23 and dia<=30 and mes==9:
+    print("Libra")
+elif dia<=22 and dia>=1 and mes==10:
+    print("Libra")
+if dia>=23 and dia<=31 and mes==10:
+    print("Escorpio")
+elif dia<=21 and dia>=1 and mes==11:
+    print("Escorpio")
+if dia>=22 and dia<=30 and mes==11:
+    print("Sagitario")
+elif dia<=21 and dia>=1 and mes==12:
+    print("Sagitario")
+    
